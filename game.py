3501eb6b2f733c83a456ebc1514c9363414d7406
doulos05 <doulos05@gmail.com>
2016-06-05 08:48:57 -0400

    Commenting Code

--- game.py
+++ game.py
@@ -5,30 +5,32 @@ from player import Player
 class Game():
   def __init__(self):
     self.main = curses.initscr()
-    self.height, self.width = self.main.getmaxyx()
+    self.height, self.width = self.main.getmaxyx() #Gets screensize. Split this into its own function called on screen resize
     self.main.border(0)
     curses.noecho()
-    self.main.keypad(1)
-    self.player = Player(int(floor(self.height/2)), int(floor(self.width/2)))
-    self.main.addch(self.player.y, self.player.x, self.player.disp)
+    curses.curs_set(0) #hide flashing cursor
+    self.main.keypad(1) #Enables keystrokes like KEY_UP, KEY_DOWN, etc
+    self.player = Player(int(floor(self.height/2)), int(floor(self.width/2))) #See player.py for class description
+    self.main.addch(self.player.y, self.player.x, self.player.disp) #Make this a draw_creature function later
     self.main_loop()
    
   def main_loop(self):
+    #This will catch and handle all keystrokes. Not too happy with if,elif,elif or case. Use a dict lookup eventually
     while 1:
       c = self.main.getch()
       if c == ord('p'):
-        self.main.addstr(1,1,"It works!")
+        self.main.addstr(self.height-1,1,self.player.description)
       elif c == ord('q'):
         self.save_game()
       elif c == curses.KEY_HOME:
         x = y = 0
   
   def save_game(self):
-    curses.nocbreak()
-    self.main.keypad(0)
-    curses.echo()
-    curses.endwin()
+    curses.nocbreak() #Step 1 to restore normal terminal function
+    self.main.keypad(0) #Step 2 to restore normal terminal function
+    curses.echo() #Step 3 to restore normal termainal function 
+    curses.endwin() #Sep 4 to restore normal terminal function. These 4 must be called before exit or the terminal goes weird.
     sys.exit()
 
 if __name__ == "__main__":
-  g = Game()
+  g = Game() #Should I put the main loop outside the Game Object?
--- player.py
+++ player.py
@@ -1,5 +1,8 @@
 class Player():
-  def __init__(self,y,x):
+  """Player will eventually subclass a MapObject class. That class will have every variable objects on the map have in common.
+  Definitely that will include x,y coordinates, a description, and a display character."""
+  def __init__(self,y,x, disp="@", description="A stout warrior of courage and might"):
     self.y = y
     self.x = x
     self.disp = "@"
+    self.description = description
