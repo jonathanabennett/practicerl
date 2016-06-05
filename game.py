20780e1538c77cf4fa431f818169a4131a92a758
doulos05 <doulos05@gmail.com>
2016-06-05 09:23:54 -0400

    Added keypad movement. N/S/E/W only using arrow keys atm.

--- game.py
+++ game.py
@@ -18,13 +18,31 @@ class Game():
     #This will catch and handle all keystrokes. Not too happy with if,elif,elif or case. Use a dict lookup eventually
     while 1:
       c = self.main.getch()
+      self.main.addstr(self.height-1,1,str(c))
       if c == ord('p'):
         self.main.addstr(self.height-1,1,self.player.description)
+      if c == curses.KEY_UP:
+        self.move_object(self.player, "North")
+      if c == curses.KEY_DOWN:
+        self.move_object(self.player, "South")
+      if c == curses.KEY_LEFT:
+        self.move_object(self.player, "West")
+      if c == curses.KEY_RIGHT:
+        self.move_object(self.player, "East")
       elif c == ord('q'):
         self.save_game()
       elif c == curses.KEY_HOME:
         x = y = 0
-  
+  def move_object(self, thing, direction):
+    curx = thing.x
+    cury = thing.y
+    newy, newx = thing.move(direction)
+    if cury != newy or curx != newx:
+      self.main.addch(cury, curx, " ")
+      self.main.addch(newy, newx, thing.disp)
+      return True
+    else:
+      return False
   def save_game(self):
     curses.nocbreak() #Step 1 to restore normal terminal function
     self.main.keypad(0) #Step 2 to restore normal terminal function
--- player.py
+++ player.py
@@ -1,3 +1,5 @@
+directions = {"North":(-1,0), "South":(1,0), "East":(0,1), "West":(0,-1)}
+
 class Player():
   """Player will eventually subclass a MapObject class. That class will have every variable objects on the map have in common.
   Definitely that will include x,y coordinates, a description, and a display character."""
@@ -6,3 +8,11 @@ class Player():
     self.x = x
     self.disp = "@"
     self.description = description
+
+  def move(self, direction, map=[]): #Map will be for collision detection. Put here to simplify wall walking implementation
+    newy = self.y + directions[direction][0]
+    newx = self.x + directions[direction][1]
+    if True:
+      self.x = newx
+      self.y = newy
+    return self.y, self.x
